9e7de417ac290005efb9ba7f8864e77ff24d96d8
Kevin Perez <kevindperezm@gmail.com>
2015-06-09 22:13:41 -0500

    añadir una prueba fallida para travis
    Con esta prueba, pretendo demostrar que la configuración de Travis está
    completa.

--- app/src/androidTest/java/mx/edu/integadora3/utem/tourguide/ApplicationTest.java
+++ app/src/androidTest/java/mx/edu/integadora3/utem/tourguide/ApplicationTest.java
@@ -3,11 +3,17 @@ package mx.edu.integadora3.utem.tourguide;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import junit.framework.TestResult;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testEverythingIsRight() {
+        assertTrue(false);
     }
 }

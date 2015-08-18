18d4a754905ad8507a01bd74d2fc01879ccfd519
Kevin Perez <kevindperezm@gmail.com>
2015-08-18 16:33:22 -0500

    remover clase de nombre repetida en DatosUsuario

--- app/src/main/java/com/tourguide/activities/DatosUsuario.java
+++ app/src/main/java/com/tourguide/activities/DatosUsuario.java
@@ -11,78 +11,36 @@ import android.widget.Spinner;
 
 import com.tourguide.R;
 
-
 public class DatosUsuario extends Activity {
 
-    public Spinner spinner;
-    public EditText nombre;
-    public EditText apellido;
-    public EditText correo;
-    public Button guardar;
-    private Configuration conf;
+  public Spinner spinner;
+  public EditText nombre;
+  public EditText apellido;
+  public EditText correo;
+  public Button guardar;
+  private Configuration conf;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_datos_usuario);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_datos_usuario);
 
-        nombre = (EditText) findViewById(R.id.nombre);
-        apellido = (EditText) findViewById(R.id.apellido);
-        correo = (EditText) findViewById(R.id.correo);
+    nombre = (EditText) findViewById(R.id.nombre);
+    apellido = (EditText) findViewById(R.id.apellido);
+    correo = (EditText) findViewById(R.id.correo);
 
-    }
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tgdatos_usuario, menu);
-        return true;
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_tgdatos_usuario, menu);
+    return true;
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    return super.onOptionsItemSelected(item);
+  }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    public class DatosUsuario{
-        private String nombre;
-        private String apellido;
-        private String correo;
-
-        public DatosUsuario(String nombre, String apellido, String correo){
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.correo = correo;
-        }
-
-        public String getNombre(){
-            return nombre;
-        }
-
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
-
-        public String getApellido(){
-            return apellido;
-        }
-
-        public void setApellido(String apellido) {
-            this.apellido = apellido;
-        }
-
-        public String getCorreo(){
-            return correo;
-        }
-
-        public void setCorreo(String correo) {
-            this.correo = correo;
-        }
-    }
 }

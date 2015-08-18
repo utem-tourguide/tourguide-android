ba8ec85f06ed5598100123ceb69f9d212f764d81
Kevin Perez <kevindperezm@gmail.com>
2015-08-18 18:33:16 -0500

    cambiar nombre a actividad de datos de usuario

--- app/src/main/java/com/tourguide/activities/Perfil.java
+++ app/src/main/java/com/tourguide/activities/Perfil.java
@@ -1,40 +1,33 @@
 package com.tourguide.activities;
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 import com.tourguide.R;
 
-public class DatosUsuario extends Activity {
+public class Perfil extends Activity {
 
-  public Spinner spinner;
   public EditText nombre;
   public EditText apellido;
   public EditText correo;
-  public Button guardar;
-  private Configuration conf;
 
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_datos_usuario);
+    setContentView(R.layout.activity_perfil);
 
     nombre = (EditText) findViewById(R.id.nombre);
     apellido = (EditText) findViewById(R.id.apellido);
     correo = (EditText) findViewById(R.id.correo);
-
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_datos_usuario, menu);
+    getMenuInflater().inflate(R.menu.menu_perfil, menu);
     return true;
   }
 

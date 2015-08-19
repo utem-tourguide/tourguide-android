d88a9266303341bbac16212d19cd4c8f2bfbb67f
Kevin Perez <kevindperezm@gmail.com>
2015-08-18 22:00:07 -0500

    añadir clase para almacenar constantes

---
+++ app/src/main/java/com/tourguide/support/Constants.java
@@ -0,0 +1,11 @@
+package com.tourguide.support;
+
+/**
+ * Almacena constantes que pueden accederse desde toda la aplicación y que no pertenecen a alguna
+ * clase en particular.
+ */
+public class Constants {
+
+  public static final String BACKEND_URL = "http://tourguide-admin.herokuapp.com";
+
+}

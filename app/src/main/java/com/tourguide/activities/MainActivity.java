8b3a2a1f9efc2e147c93ed72d426159dea36eb1e
Kevin Perez <kevindperezm@gmail.com>
2015-08-19 05:55:32 -0500

    añadir acceso a perfil en actividad principal

--- app/src/main/java/com/tourguide/activities/MainActivity.java
+++ app/src/main/java/com/tourguide/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tourguide.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,20 +17,18 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-
         return super.onOptionsItemSelected(item);
     }
+
+    public void onPerfilMenuItemClick(MenuItem item) {
+        startActivity(new Intent(this, PerfilActivity.class));
+    }
+
 }

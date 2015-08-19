4178a8ddccd6fe46a4aa96377f763a588b2e3e38
Kevin Perez <kevindperezm@gmail.com>
2015-08-18 19:25:30 -0500

    rediseñar pantalla de perfil de usuario
    Esto rediseña la pantalla del perfil del usuario y también añade un botón
    para guardar en el ActionBar.

--- app/src/main/java/com/tourguide/activities/Perfil.java
+++ app/src/main/java/com/tourguide/activities/Perfil.java
@@ -4,25 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 
 import com.tourguide.R;
 
 public class Perfil extends Activity {
 
-  public EditText nombre;
-  public EditText apellido;
-  public EditText correo;
-
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_perfil);
-
-    nombre = (EditText) findViewById(R.id.nombre);
-    apellido = (EditText) findViewById(R.id.apellido);
-    correo = (EditText) findViewById(R.id.correo);
   }
 
   @Override

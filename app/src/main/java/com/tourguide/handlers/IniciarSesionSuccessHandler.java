9b1a07bb813de894dc7aaea9b0b028f008089ecb
Kevin Perez <kevindperezm@gmail.com>
2015-08-20 17:45:33 -0500

    guardar id de login tras iniciar sesión

--- app/src/main/java/com/tourguide/handlers/IniciarSesionSuccessHandler.java
+++ app/src/main/java/com/tourguide/handlers/IniciarSesionSuccessHandler.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 
 import com.tourguide.activities.LoginActivity;
 import com.tourguide.activities.MainActivity;
+import com.tourguide.managers.LoginManager;
+import com.tourguide.support.Constants;
 
 public class IniciarSesionSuccessHandler extends BackendResponseHandler {
 
@@ -15,6 +17,8 @@ public class IniciarSesionSuccessHandler extends BackendResponseHandler {
 
   @Override
   public void handle() {
+    LoginManager.setLoginId(Constants.getUsuario().getId());
+
     actividad.startActivity(new Intent(actividad, MainActivity.class));
 
     actividad.finish();
